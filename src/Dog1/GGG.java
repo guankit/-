47c8d13c06fa46f01c1b7dbbc2e979b454a053e5
guankit <1434638536@qq.com>
2018-01-02 16:48:49 +0800

    1416

---
+++ src/Dog1/GGG.java
@@ -0,0 +1,23 @@
+package Dog1;
+import java.util.Scanner;
+
+public class GGG {
+
+		public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("输入一个数");
+		
+		int a=sc.nextInt();
+		
+		shuchu(a);
+		
+
+		}
+
+		private static void shuchu(int b) {
+         for(int i=1;i<=b;i++){
+         System.out.println(i);
+		}
+		}
+
+}
